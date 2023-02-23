3e2358c676701b4beca7c48de2760cc5ad67399e
Max Kiefer <81292206+Maxkie1@users.noreply.github.com>
2023-02-23 09:46:39 +0100

    feat: initialize main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+"""Appplication to categorize student's learning style.
+
+This application retrieves activity log data from Moodle and puts students into the dimensions of the felder-silverman learning style model based on their personal acitivity log.
+"""
